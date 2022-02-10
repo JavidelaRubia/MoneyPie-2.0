17d79077c064422e28d7d57ce5bcc838e240c20e
JavidelaRubia <javidelarubia@gmail.com>
2022-02-10 13:09:49 +0100

    Login

---
+++ JS/login.js
@@ -0,0 +1,55 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+  document.getElementById("login").addEventListener("click", () => {
+    let usuario = document.getElementById("usuario").value;
+    let pass = document.getElementById("password").value;
+    let valido = true;
+
+    let errores_DOM = document.getElementById("erroresLogin");
+    const errores = {
+        nombre:"",
+        pass:"",
+        credencial:""
+    };
+
+    function imprimeErrores() {
+        let text = "";
+        Object.keys(errores).forEach((key) => {
+          if (errores[key]) {
+            text += `<p class="bg-danger">${errores[key]}</p>`;
+          }
+        });
+        errores_DOM.innerHTML = "";
+        errores_DOM.innerHTML = text;
+    }
+
+    console.log("Hola");
+    console.log("Usuario: "+usuario);
+    console.log("Pass: "+pass);
+
+
+    if (usuario == "") {
+      errores.nombre += "Usuario vacio";
+      valido = false;
+    }
+    if (pass == "") {
+      errores.pass += "Contraseña vacia";
+      valido = false;
+    }
+    if (valido) {
+      if (usuario != pass) {
+        errores.credencial += "Login errorneo";
+        
+      } else {
+        window.location.href = "index.html";
+      }
+    }
+    imprimeErrores();
+
+    
+  });
+  
+
+});
+
+
+    
